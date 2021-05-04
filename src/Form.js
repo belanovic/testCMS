eb46280dcfdd15c5640fc72c4a062c3f220e731e
Goran Belanovic <Goran Belanovic>
2021-05-04 16:13:16 +0200

    'msg'

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,4 @@
-import react, { useState, useEffect, useContext } from 'react';
+import react, { useState, useEffect, useContext, useRef } from 'react';
 import { context } from './newsContext';
 import './styleForm.css';
 import {registerUser} from './getUser.js';
@@ -21,9 +21,12 @@ export default function Form() {
     const [email, setEmail] = useState('');
 
 
-    const handleChange = (e, setFunc) => {
+    const handleChange = (e, setFunc, limit) => {
         const value = e.target.value;
         setFunc(value);
+       /*  if(value.length === limit) {
+            e.target.style.visibility = "hidden"
+        } */
     }
 
     const handleClickTab = (tab) => {
@@ -80,7 +83,7 @@ export default function Form() {
                             </div>
 
                             <div 
-                                className="form-element 3"
+                                className="form-element"
                                 style = {{display: signUpisActive? 'block' : 'none'}}
                             >
                                 <label htmlFor="firstName" className="firstName-label">Ime</label>
@@ -89,10 +92,16 @@ export default function Form() {
                                     className="firstName-input" 
                                     type="text"
                                     value = {firstName}
-                                    onChange = {(e) => handleChange(e, setFirstName)}
-
-                                    maxLength = "6"
+                                    onChange = {(e) => handleChange(e, setFirstName, 3)}
                                 ></input>
+                                <div className = "form-element-condition firstname">
+                                    <span 
+                                        style = {{
+                                            visibility: firstName.length >= 3 || firstName.length === 0 ? 
+                                                'hidden' : 'visible'
+                                            }}
+                                    >Ime mora imati najmanje 3 karaktera</span>
+                                </div>
                             </div>
 
                             <div 
@@ -105,8 +114,16 @@ export default function Form() {
                                     className="lastName-input" 
                                     type="text" 
                                     value = {lastName}
-                                    onChange = {(e) => handleChange(e, setLastName)}
+                                    onChange = {(e) => handleChange(e, setLastName, 3)}
                                 ></input>
+                                <div className = "form-element-condition lastname">
+                                    <span 
+                                        style = {{
+                                            visibility: lastName.length >= 3 || lastName.length === 0 ? 
+                                                'hidden' : 'visible'
+                                            }}
+                                    >Prezime mora imati najmanje 3 karaktera</span>
+                                </div>
                             </div>
 
                             <div 
@@ -119,8 +136,17 @@ export default function Form() {
                                     className="usernameSignIn-input" 
                                     type="text"
                                     value = {usernameSignIn}
-                                    onChange = {(e) => handleChange(e, setUsernameSignIn)}
+                                    onChange = {(e) => handleChange(e, setUsernameSignIn, 3)}
                                 ></input>
+    
+                                <div className = "form-element-condition usernameSignIn">
+                                    <span 
+                                        style = {{
+                                            visibility: usernameSignIn.length >= 3 || usernameSignIn.length === 0 ? 
+                                                'hidden' : 'visible'
+                                            }}
+                                    >Korisničko ime mora imati najmanje 3 karaktera</span>
+                                </div>
                             </div>
                             <div 
                                 className="form-element usernameSignUp"
@@ -132,8 +158,17 @@ export default function Form() {
                                     className="usernameSignUp-input" 
                                     type="text"
                                     value = {usernameSignUp}
-                                    onChange = {(e) => handleChange(e, setUsernameSignUp)}
+                                    onChange = {(e) => handleChange(e, setUsernameSignUp, 3)}
                                 ></input>
+                
+                                <div className = "form-element-condition usernameSignUp">
+                                    <span 
+                                        style = {{
+                                            visibility: usernameSignUp.length >= 3 || usernameSignUp.length === 0 ? 
+                                                'hidden' : 'visible'
+                                            }}
+                                    >Korisničko ime mora imati najmanje 3 karaktera</span>
+                                </div>
                             </div>
 
                             <div 
@@ -146,8 +181,17 @@ export default function Form() {
                                     className="paswordSignIn-input" 
                                     type="text" 
                                     value = {passwordSignIn}
-                                    onChange = {(e) => handleChange(e, setPasswordSignIn)}
+                                    onChange = {(e) => handleChange(e, setPasswordSignIn, 6)}
                                 ></input>
+                
+                                <div className = "form-element-condition passwordSignIn">
+                                    <span 
+                                        style = {{
+                                            visibility: passwordSignIn.length >= 6 || passwordSignIn.length === 0 ? 
+                                                'hidden' : 'visible'
+                                            }}
+                                    >Šifra mora imati najmanje 6 karaktera</span>
+                                </div>
                             </div>
                             <div 
                                 className="form-element paswordSignUp"
@@ -159,8 +203,17 @@ export default function Form() {
                                     className="paswordSignUp-input" 
                                     type="text" 
                                     value = {passwordSignUp}
-                                    onChange = {(e) => handleChange(e, setPasswordSignUp)}
+                                    onChange = {(e) => handleChange(e, setPasswordSignUp, 6)}
                                 ></input>
+
+                                <div className = "form-element-condition passwordSignUp">
+                                    <span 
+                                        style = {{
+                                            visibility: passwordSignUp.length >= 6 || passwordSignUp.length === 0 ? 
+                                                'hidden' : 'visible'
+                                            }}
+                                    >Šifra mora imati najmanje 6 karaktera</span>
+                                </div>
                             </div>
 
                             <div 
@@ -173,8 +226,17 @@ export default function Form() {
                                     className="email-input" 
                                     type="email"
                                     value = {email}
-                                    onChange = {(e) => handleChange(e, setEmail)}
+                                    onChange = {(e) => handleChange(e, setEmail, 10)}
                                 ></input>
+         
+                                <div className = "form-element-condition email">
+                                    <span 
+                                        style = {{
+                                            visibility: email.length >= 10 || email.length === 0 ? 
+                                                'hidden' : 'visible'
+                                            }}
+                                    >Email mora imati najmanje 10 karaktera</span>
+                                </div>
                             </div>
                             <div className="form-send">
                                 <button 
